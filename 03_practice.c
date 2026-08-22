2e42bf3fa7044c971cef9b39776b08750bc67dc0
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-23 03:18:29 +0545

    Add floating-point arithmetic operations

---
+++ 03_practice.c
@@ -0,0 +1,15 @@
+/* Note: We cannot use '%' operator on floating points numbers */
+
+#include <stdio.h>
+
+int main(void) {
+
+  float a = 12.4, b = 3.8;
+
+  printf("Sum = %.2f\n", a + b);
+  printf("Difference = %.2f\n", a - b);
+  printf("Product = %.2f\n", a * b);
+  printf("Quotient = %.2f\n", a / b);
+
+  return 0;
+}
