a3c5751f59e022e1d03d4150ba9eed78dd48f46d
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-23 14:08:21 +0545

    Add relational operators practice

---
+++ 06_practice.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main(void) {
+
+ int a, b;
+ printf("Enter the values for a and b : ");
+ scanf("%d %d", &a, &b);
+
+ if(a < b)
+  printf("%d is less than %d\n", a, b);
+
+ if(a <= b)
+  printf("%d is less than or equal to %d\n", a, b);
+
+ if(a == b) 
+  printf("%d is equal to %d\n", a, b);
+
+ if(a != b)
+  printf("%d is not equal to %d\n", a, b);
+
+ if(a > b)
+  printf("%d is greater than %d\n", a, b);
+
+ if(a >= b)
+  printf("%d is greater than or equal to %d\n", a, b);
+
+  return 0;
+}
