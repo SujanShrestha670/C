870771715a4665554692402eded777d78573800b
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-23 14:14:00 +0545

    Add program to find larger of two numbers using conditional operators

---
+++ 07_practice.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void) {
+
+  int a, b, max;
+  printf("Enter values for a & b : ");
+  scanf("%d %d", &a, &b);
+
+  max = (a > b) ? a : b;
+
+  printf("Larger of %d and %d is %d\n", a, b, max);
+
+  return 0;
+}
