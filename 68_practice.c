2901471967f3ebee7c779e7447ddbaecf438b94d
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-29 20:11:59 +0545

    Add pattern program 11

---
+++ 68_practice.c
@@ -0,0 +1,24 @@
+/*
+
+1 2 3 4 5
+1 2 3 4
+1 2 3
+1 2
+1
+ 
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("\n");
+  for(int i = 5; i > 0; i--) {
+    for(int j = 1; j <= i; j++) {
+      printf("%d ", j);
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
