4a3ab1c4d196ab76f15864dec460a57c543a1053
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-30 09:12:07 +0545

    Add pattern program 19

---
+++ 76_practice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void) {
+
+  printf("\n");
+  for(int i = 1; i <= 5; i++) {
+    for(int j = 5 - i; j > 0; j--) {
+      printf(" ");
+    }
+
+    for(int k = 1; k <= i; k++) {
+      printf("%d", i);
+    }
+    
+    if(i >= 2) {
+      for(int l = 1; l < i; l++) {
+        printf("%d", i);
+      }
+    }
+    printf("\n");
+  }
+
+  return 0;
+}
