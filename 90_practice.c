64a8a4228c73ab695170b13688d08f6b548a6fc0
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-08-30 15:29:21 +0545

    Add pattern program 33

---
+++ 90_practice.c
@@ -0,0 +1,59 @@
+/*
+ 
+    5
+   545
+  54345
+ 5432345
+543212345
+ 5432345
+  54345
+   545
+    5
+
+*/
+
+#include <stdio.h>
+
+int main(void) {
+
+  printf("\n");
+  for(int i = 1; i <= 9; i += 2) {
+    for(int j = 1; j <= (9 - i) / 2; j++) {
+      printf(" ");
+    }
+
+    int l = 5;
+    int direction = -1;
+    int spaces = (9 - i) / 2;
+
+    for(int k = 1; k <= i; k++) {
+      printf("%d", l);
+      if(l == (spaces + 1)) {
+        direction = 1;
+      }
+      l += direction;
+    }
+    printf("\n");
+  }
+
+  for(int i = 7; i > 0; i -= 2){
+    for(int j = 1; j <= (9 - i) / 2; j++) {
+      printf(" ");
+    }
+
+    int l = 5;
+    int direction = -1;
+    int spaces = (9 - i) / 2;
+
+    for(int k = 1; k <= i; k++) {
+      printf("%d", l);
+      if(l == (spaces + 1)) {
+        direction = 1;
+      }
+      l += direction;
+    }
+    printf("\n");
+  } 
+
+  return 0;
+}
