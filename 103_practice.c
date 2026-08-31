34c022373cd8b93807bc71447a7f61ba2d4a052d
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-09-01 01:06:09 +0545

    Add program to print prime numbers from 1 to 99

---
+++ 103_practice.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void) {
+
+  printf("The prime numbers from 1 to 99 are :\n");
+  for(int i = 2; i <= 99; i++) {
+
+    int isPrime = 1;
+
+    for(int j = 2; j <= sqrt(i); j++) {
+      if(i % j == 0) {
+        isPrime = 0;
+        break;
+      }
+    }
+
+    if(isPrime) {
+      printf("%d\n", i);
+    }
+  }
+
+
+  return 0;
+}
