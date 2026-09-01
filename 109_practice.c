df0c734aeee978c0cbeb430ecb6c9c207379658d
SujanShrestha670 <sujanshresthaa670@gmail.com>
2026-09-01 19:13:27 +0545

    Add pattern program 35

--- 109_practice.c
+++ 109_practice.c
@@ -1,27 +1,26 @@
 /*
- 
-* * * * *
- * * * * *
-* * * * *
- * * * * *
 
+*******
+*******
+*******
+*******
+*******
+*******
+*******
+ 
 */
 
 #include <stdio.h>
 
 int main(void) {
 
-  int number, i = 1;
+  int number;
   printf("Enter a number : ");
   scanf("%d", &number);
 
   for(int i = 1; i <= number; i++) {
     for(int j = 1; j <= number; j++) {
-      if(i % 2 == 0) {
-        printf(" *");
-      }else {
-        printf("* ");
-      }
+      printf("*");
     }
     printf("\n");
   }
